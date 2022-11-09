693c5354c4cfc84ccde77dad0c48f0682b148ad8
IBOBRO <ibrohimlogos@gmail.com>
2022-11-09 23:30:06 +0500

    big changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-console.log("lol");
+function op(){
+    var correctPass=document.getElementById("password").value;
+   if(correctPass == 1)
+       document.location="second.html";
+       
+   
+    else{
+        alert("invaild information")
+   }
+}  
+  
