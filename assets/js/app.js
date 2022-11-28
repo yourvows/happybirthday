32d1cde79d0b42ccf32ce396ce838578e759069e
IBOBRO <ibrohimlogos@gmail.com>
2022-11-28 18:11:45 +0500

    advision deploy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,18 @@
-function op(){
-    var correctPass=document.getElementById("password").value;
-   if(correctPass == 1)
-       document.location="second.html";
-       
-   
-    else{
+function op() {
+    var correctPass = document.getElementById("password").value;
+    if (correctPass == 1)
+        document.location = "../pages/second.html";
+    else {
         alert("invaild information")
-   }
-}  
-  
+    }
+}
+
+function third() {
+    var correctPass = document.getElementById("password3").value;
+    if (correctPass == 'твори'||'Твори')
+    console.log(123);
+        // document.location = "../pages/second.html";
+    else {
+        alert("invaild information")
+    }
+}
