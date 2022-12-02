44361b1fa8fbf90e9ea1d91e8dc818596dd98b6e
yourvows <ibrohim2133@gmail.com>
2022-12-02 20:03:05 +0500

    pages created

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,8 +13,53 @@ function third() {
     console.log(correctPass);
     if (correctPass == 'твори' || correctPass == 'Твори') {
         console.log(123);
-        document.location = "../pages/second.html";
+        document.location = "../pages/fourth.html";
     }
+    else {
+        alert(404)
+    }
+}
+//pass 4
+function fourth() {
+    let correctPass = document.getElementById("password4").value;
+    console.log(correctPass);
+    if (correctPass == 'крылья' || correctPass == 'Крылья') {
+        console.log(123);
+        document.location = "../pages/fifth.html";
+    }
+    else {
+        alert(404)
+    }
+}
+//pass 5
+function fifth() {
+    let correctPass = document.getElementById("password5").value;
+    console.log(correctPass);
+    if (correctPass == 'люби' || correctPass == 'Люби') {
+        console.log(123);
+        document.location = "../pages/sixth.html";
+    }
+    else {
+        alert(404)
+    }
+}
+//pass 6
+function sixth() {
+    let correctPass = document.getElementById("password6").value;
+    console.log(correctPass);
+    if (correctPass == 'истинная красота' || correctPass == 'Истинная красота') {
+        console.log(123);
+        document.location = "../pages/seventh.html";
+    }
+    else {
+        alert(404)
+    }
+}
+//pass 7
+function op() {
+    let correctPass = document.getElementById("password").value;
+    if (correctPass == 'случайности'|| correctPass == 'Случайности')
+        document.location = "./assets/pages/second.html";
     else {
         alert(404)
     }
